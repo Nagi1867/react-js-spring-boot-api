7dcc3e4c99c6282baf1336c045419e24f8c357a5
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-11-10 22:42:53 -0300

    adicionando redirecionamento quando o token expirar

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080',
-})
+  baseURL: "http://localhost:8080",
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      alert("Sessão expirada. Faça login novamente!");
+      localStorage.clear();
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
